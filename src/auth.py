e1cab67b14bd94d6b456bd2e74984ba8f1e003bb
Hexye <65314629+HexyeDEV@users.noreply.github.com>
2022-04-19 15:43:22 +0200

    update auth

--- src/auth.py
+++ src/auth.py
@@ -8,3 +8,14 @@ def login():
         return render_template("login.html")
     else:
         room_code = request.form.get("room")
+        pw = request.form.get("password")
+        # TODO: Check room and password hash
+
+@auth.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        room_code = 0 # TODO: Randomize room code
+        pw = request.form.get("password")
+        # TODO: Add room and pasword hash to the database
